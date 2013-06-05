69c2be1b0eecb2dde72b224b0ee241e4342e5fb2
Mark Miyashita <negativetwelve@gmail.com>
2013-06-04 23:56:09 -0700

    First automated commit

---
+++ _deploy/public/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+  var relative = window.location.pathname;
+  var absolute = $(location).attr('href');
+  if (relative == '/' && absolute.slice(-1) != '/') {
+    section = absolute.split('#')[1].toLowerCase();
+    if (section != 'projects') {
+      loadSection(section);
+    } else {
+      loadProjects();
+    }
+  } else if (relative != '/') {
+    scrollToContent();
+  }
+  
+  $('.load-projects').click(function() {
+    loadProjects();
+  });
+  
+  //$('.resume .row.company-modal').click(function() {
+    //$('#' + $(this).attr('data-reveal-id')).reveal();
+  //});
+
+  $('.load-item').click(function() {
+    loadSection($(this).context.text);
+  });
+  
+  $('.load-resume').click(function() {
+    loadSection('resume');
+  })
+  
+  $('.load-contact').click(function() {
+    loadSection('contact');
+  });
+});
+
+function scrollToContent() {
+  $('html, body').animate({
+    scrollTop: $("#content").offset().top
+  }, 10);
+};
+
+function loadSection(location) {
+  var loc = location.toLowerCase();
+  pageTracker._trackPageview('/#' + loc);
+  $('section').stop().slideUp();
+  var section = $('.' + loc);
+  $(section).stop().slideDown(function() {
+    scrollToContent();
+  });
+};
+
+function loadProjects() {
+  pageTracker._trackPageview('/#projects');
+  $('section').stop().slideUp();
+  var section = $('.resume');
+  $(section).stop().slideDown(function() {
+    $('html, body').animate({
+      scrollTop: $("#projects").offset().top
+    }, 200);
+  });
+};
